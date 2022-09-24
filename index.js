bb3d217a7fb620101062b4741d9f91250c0704a9
Nikeex <dileshchouhan86@gmail.com>
2022-09-24 19:20:25 +0530

    update some blog component

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const mobile_nav = document.querySelector(".mobile-navbar-btn");
 const nav_header = document.querySelector(".header");
-
 const toggleNavbar = () => {
   nav_header.classList.toggle("active");
 };
